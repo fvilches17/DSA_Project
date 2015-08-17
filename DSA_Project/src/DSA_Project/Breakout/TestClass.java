3da28e3528a81686a243568790bed748134e2082
Olga Yaroshenko <yaroshenko.olga@gmail.com>
2015-08-17 21:23:16 +1200

    Resolved conflict testclass

--- DSA_Project/src/DSA_Project/Breakout/TestClass.java
+++ DSA_Project/src/DSA_Project/Breakout/TestClass.java
@@ -4,12 +4,16 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.io.*;
 import java.util.*;
+import java.util.Random;
+import javax.swing.JFrame;
+
 
 public class TestClass {
 
     public static void main(String[] args) {
+        /*
         ArrayList<Ball> balls = new ArrayList<>();
-        Ball ball = new Ball(50.0, Color.BLACK);
+        Ball ball = new Ball(5.0, 5.0, 5.0, 5.0, 5.0, 5.0, Color.yellow);
         balls.add(ball);
         FileAccessor.writeArrayIntoFile(balls, "balls.bin");
 
@@ -17,6 +21,12 @@ public class TestClass {
 
         balls2 = (ArrayList<Ball>) FileAccessor.getArrayFromFile("balls.bin");
         System.out.println(balls2.get(0));
+        */
+        JFrame frame = new JFrame();
+        Game game = new Game(frame, 1);
+        game.start();
+        
+        
     }
 }
     
